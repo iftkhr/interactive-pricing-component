d1a4de84c35a04d8bd933c93f5687a8e9eac020b
Iftakhar Kaunain Ashhar <iftakhar.ashhar@gmail.com>
2022-08-13 01:28:15 +0530

    Final touches

--- src/App/App.js
+++ src/App/App.js
@@ -18,6 +18,18 @@ function App() {
 	return (
 		<ThemeContext.Provider value={{ theme, setTheme }}>
 			<div className="app-container" id={theme}>
+				<div className="theme-toggle" onClick={toggleTheme}>
+					{mode}
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 512 512"
+					>
+						<path
+							d="M512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 64V448C362 448 448 362 448 256C448 149.1 362 64 256 64z"
+							fill="#fff"
+						/>
+					</svg>
+				</div>
 				<div className="circles">
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
@@ -49,18 +61,6 @@ function App() {
 					</div>
 				</div>
 				<Pricing />
-				<div className="theme-toggle" onClick={toggleTheme}>
-					{mode}
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 512 512"
-					>
-						<path
-							d="M512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 64V448C362 448 448 362 448 256C448 149.1 362 64 256 64z"
-							fill="#fff"
-						/>
-					</svg>
-				</div>
 			</div>
 		</ThemeContext.Provider>
 	);
